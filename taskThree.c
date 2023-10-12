d1565bd362e2c39279c3a8815322925a16a6a643
babadzakich <doctorare@mail.ru>
2023-10-12 14:34:56 +0700

    Solved task with dinamic memory allocation

--- taskThree.c
+++ taskThree.c
@@ -4,55 +4,71 @@
 
 int main()
 {
-    freopen("output.txt", "w", stdout);
+    
     freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
     int amountOfCalls, amountOfLines = 0;
-    char **identificators;
     fscanf(stdin,"%d",&amountOfCalls);
+    char **identificators = (char**)malloc((amountOfCalls) * sizeof(char*));
+    
+    for (int step = 0; step < amountOfCalls; ++step)
+    {
+        identificators[step] = NULL;
+    }
     for (int step = 0; step < amountOfCalls; ++step)
     {   
-        int currentCall;
+        int currentCall, identifier;
         fscanf(stdin, "%d", &currentCall);
         if (currentCall == 0)
         {
             int lineLength;
             fscanf(stdin, "%d", &lineLength);
-            char * line = (char*)malloc(lineLength);
-            fscanf(stdin, "%s", line);
-            *(identificators+amountOfLines++) = line;
+            identificators[amountOfLines] = (char*)malloc(sizeof(char) * (lineLength+1));
+            fscanf(stdin, "%s", identificators[amountOfLines]);
+            amountOfLines++;
         }
         else if (currentCall == 1)
         {
-            int identifier;
             fscanf(stdin, "%d", &identifier);
             free(*(identificators+identifier));
+            *(identificators+identifier) = NULL;
         }
         else if (currentCall == 2)
         {
-            int identifier;
             fscanf(stdin, "%d", &identifier);
-            printf("%s", *(identificators+identifier));
+            int lineLength = strlen(identificators[identifier]);
+            for (int chrpos = 0; chrpos < lineLength; ++chrpos)
+            {
+                fprintf(stdout, "%c", identificators[identifier][chrpos]);
+            }
+            fprintf(stdout, "\n");
+            //fprintf(stdout, "%s\n", *(identificators+identifier));
         }
         else
         {
-            int identifier;
-            char simbol;
-            fscanf(stdin, "%d %c", &identifier, &simbol);
-            int step2 = 0;
-            int counter = 0;
-            while (*(*(identificators+identifier)+step) != 0)
+            char symbol;
+            fscanf(stdin, "%d %c", &identifier, &symbol);
+            int counter = 0, lineLength;
+            lineLength = strlen(*(identificators+identifier));
+            for (int step2 = 0; step2 < lineLength; step2++) 
             {
-                if (*(identificators[identifier]+step) == simbol)
+                if (identificators[identifier][step2] == symbol)
                 {
                     counter ++;
                 }
-                step2++;
             }
-            printf("%d", counter);
+            fprintf(stdout, "%d\n", counter);
         }
     }
-
-    
+    for (int step = 0; step < amountOfLines; ++step)
+    {
+        if (*(identificators+step) != NULL)
+        {
+            free(*(identificators+step));
+        }
+    }
+    free(identificators);
     fclose(stdin);
     fclose(stdout);
     return 0;
