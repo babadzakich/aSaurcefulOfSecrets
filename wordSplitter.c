966a55fbd652f7a4891eacdc6cb3c1d71062f2b6
babadzakich <doctorare@mail.ru>
2023-10-16 15:44:19 +0700

    Doing wordsplitter

--- wordSplitter.c
+++ wordSplitter.c
@@ -18,6 +18,19 @@ int isSymbolDelimiter(char symbol, const char *separators)
     }
 }
 
+char* lineSubstraction(const char* source, int* startOfLine, int* endOfLine)
+{
+    int lengthOfLine = *endOfLine - *startOfLine;
+    char* resultLine = (char*)malloc(sizeof(char) * (lengthOfLine+1));
+
+    for (int step = *startOfLine; step < *endOfLine && (*(source+step)!='\0'); step++)
+    {
+        *resultLine = *(source+step);
+        ++resultLine;
+    }
+    *resultLine = '\0';
+    return resultLine - lengthOfLine;
+}
 //tokens: структура, в которую нужно положить результаты
 //str: строка, в которой нужно искать слова
 //delims: все символы-разделители в виде строки
@@ -44,76 +57,96 @@ void tokensSplit ( Tokens *tokens , const char * str , const char * delims )
     else
     {
         tokens->arr = malloc(sizeof(char*) * tokens->num);
-        int step = 0, wordCounter = 0, letterCounter=0, isLetterFound = 0;
-        char* word = (char*)malloc(sizeof(char)*1000000);
+        int startOfWord = -1, endOfWord, step = 0, wordCounter = 0, letterCounter=0, isLetterFound = 0;
+        //char* word = (char*)malloc(sizeof(char)*1000003);
         while(str[step] != '\0')
         {
             int isDelimeter = isSymbolDelimiter(str[step], delims);
             if(!isLetterFound && !isDelimeter)
             {
                 isLetterFound = 1;
-                word[letterCounter++] = str[step];
+                startOfWord = step;
+                //word[letterCounter++] = str[step];
             }
             else if(isLetterFound && isDelimeter)
             {
                 isLetterFound = 0;
                 tokens->arr[wordCounter] = (char*)malloc(sizeof(char) * letterCounter);
-                strcpy(tokens->arr[wordCounter++], word);
-                letterCounter = 0;
-                //word[0] = '\0';
-                memset(word, '\0', sizeof(word));
+                endOfWord = step;
+
+                // for(int step2 = 0; step2 < letterCounter; step2++)
+                // {
+                //     tokens->arr[wordCounter][step2] = word[step2];
+                // }
+                tokens->arr[wordCounter++] = lineSubstraction(str, &startOfWord, &endOfWord);
+                //wordCounter++;
+                //letterCounter = 0;
+                //free(word);
             }
-            else if (isLetterFound && !isDelimeter)
-            {
-                word[letterCounter++] = str[step];
-            }
+            // else if (isLetterFound && !isDelimeter)
+            // {
+            //     //word[letterCounter++] = str[step];
+            // }
             ++step;
         }
-        if(strlen(word) != 0)
+        if(isLetterFound)
         {
-            tokens->arr[wordCounter] = (char*)malloc(sizeof(char) * letterCounter);
-            strcpy(tokens->arr[wordCounter++], word);
-            letterCounter = 0;
+            tokens->arr[wordCounter] = (char*)malloc(sizeof(char) * letterCounter+1);
+            // for(int step = 0; step < strlen(word); step++)
+            // {
+            //     tokens->arr[wordCounter][step] = word[step];
+            // }
+            // wordCounter++;
+            //strcpy(tokens->arr[wordCounter++], word);
+            endOfWord = step;
+            tokens->arr[wordCounter++] = lineSubstraction(str, &startOfWord, &endOfWord);
         }
-        free(word);
+        //free(word);
     }
 }
 
 //удаляет все ресурсы внутри tokens
 void tokensFree ( Tokens * tokens )
 {
-    for (int step = 0; step < tokens->num; ++step)
+    if (tokens->arr != NULL)
     {
-        if (tokens->arr[step] != NULL)
+        for (int step = 0; step < tokens->num; ++step)
         {
             free(tokens->arr[step]);
-            tokens->arr[step] = NULL;
         }
+        free(tokens->arr);
     }
 }
-char line[1000000];
+
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-
+    
     Tokens tokens;
     tokens.num = 0;
-    
-    char separators[4] = ".,:;";
-    
+    tokens.arr = NULL;
+
+    char separators[] = ".,:;";
+    char line[1000001];
+
     fscanf(stdin,"%s", line);
     
     tokensSplit(&tokens, line, separators);
     tokensSplit(&tokens, line, separators);
     
     fprintf(stdout, "%d\n", tokens.num);
-    for(int step = 0; step < tokens.num; ++step)
-    {
-        fprintf(stdout, "%s\n", tokens.arr[step]);
+    
+    if(tokens.num != 0)
+    {  
+        for(int step = 0; step < tokens.num; ++step)
+        {
+            fprintf(stdout, "%s\n", tokens.arr[step]);
+            //fprintf(stdout, "%s\n", tokens.arr[step]);
+        }
     }
+    
     tokensFree(&tokens);
-    free(tokens.arr);
     fclose(stdin);
     fclose(stdout);
     return 0;
